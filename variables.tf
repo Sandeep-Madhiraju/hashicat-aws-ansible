339fa935fc3e95d5aa12b6795e7ecf7702844fdf
Sandeep-Madhiraju <87055139+Sandeep-Madhiraju@users.noreply.github.com>
2021-12-23 10:29:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
-  default     = "icicidemo-2"
+  default     = "icicidemo-1"
 }
 
 variable "region" {
